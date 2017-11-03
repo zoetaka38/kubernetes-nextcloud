c6f26562217998d4e68a9a8a86afa352ee022414
zoetaka38 <rf.tk.shkp@gmail.com>
2017-11-03 14:01:04 +0900

    first commit

---
+++ k8s-deploy.sh
@@ -0,0 +1,6 @@
+kubeadm init --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.0/Documentation/kube-flannel.yml
